b130dd13970e7d156d26079f2f33be2f85a20baf
Voltaire's Ghost <yury@pulsesensor.com>
2016-05-07 15:09:14 -0400

    Initial Commit

---
+++ Tabbed3Screens/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tabbed3Screens
+//
+//  Created by Yoda Dev on 5/7/16.
+//  Copyright © 2016 Yury G. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tabbed3Screens/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tabbed3Screens
+//
+//  Created by Yoda Dev on 5/7/16.
+//  Copyright © 2016 Yury G. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
